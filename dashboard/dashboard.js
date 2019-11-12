eacfe00eacb781ae4e3027ecf2c86e428a6b575c
eh0ward <surpmusic@gmail.com>
2019-11-12 15:07:38 -0600

    jQuery ToDo App

---
+++ dashboard/dashboard.js
@@ -0,0 +1,25 @@
+$(function() {
+  $(".cBox").click(function() {
+    $(this).toggleClass("checkme");
+    $(this)
+      .siblings(".item")
+      .toggleClass("line-through");
+  });
+});
+
+$(function() {
+  let $list = $("ul");
+  let $newItemForm = $("#newItemForm");
+
+  $newItemForm.on("submit", function(e) {
+    e.preventDefault();
+    let text = $('input[type="text"]').val();
+    $list.append(`<li>${text}</li>`);
+    $('input[type="text"]').val("");
+  });
+
+  // $list.on("click", "li", function() {
+  //   let $this = $(this);
+  //   $this.remove();
+  // });
+});
