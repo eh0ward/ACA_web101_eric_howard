3069e0df867c62298ef5e2feae4a1e3f1ca10393
eh0ward <surpmusic@gmail.com>
2019-11-19 08:01:38 -0600

    geolocation

--- FITE_CLUB/fite_club.js
+++ FITE_CLUB/fite_club.js
@@ -30,3 +30,21 @@ if (navigator.geolocation) {
     }
   }
 }
+
+var myLocation = document.getElementById("my-coordinates");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(displayPosition);
+  } else {
+    myLocation.innerHTML = "Not available";
+  }
+}
+
+function displayPosition(position) {
+  myLocation.innerHTML =
+    "Longitude: " +
+    position.coords.longitude +
+    " Latitude: " +
+    position.coords.latitude;
+}
